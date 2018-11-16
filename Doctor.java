e0bd1f297510b962c417c05cba5ff2fa8adf336a
ahkhawer <36165267+ahkhawer@users.noreply.github.com>
2018-11-16 17:57:32 +0500

    Create Doctor.java

---
+++ Doctor.java
@@ -0,0 +1,15 @@
+class Doctor{
+  private String designation;
+  
+  Doctor(){
+    designation = null;
+  }
+  
+  public void setDesignation(String designation){
+    this.designation = designation;
+  }
+  
+  public String getDesignation(){
+    return this.designation;
+  }
+}
