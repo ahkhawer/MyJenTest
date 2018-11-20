a158986a1fb143aaf68f956cd092be32464fd9ae
ahkhawer <ahjavediqbal@gmail.com>
2018-11-20 11:46:13 +0500

    All files deleted

--- MainClass.java
+++
@@ -1,8 +0,0 @@
-//package com.test.example;
-
-public class MainClass{
-  public static void main(String[] args) {
-    System.out.println("Heloo");
-    System.out.println("Hello Jenkins");
-  }
-}
--- Person.java
+++
@@ -1,8 +0,0 @@
-class Person{
-  private String userName;
-  private String password;
-  Person(){
-    userName = null;
-    password = null;
-  }
-}
